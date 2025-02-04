bc9e3ca365de7bb9b391276ae21b39ecde66c686
jhkim19940830 <jhkim19940830@gmail.com>
2025-02-04 00:17:21 -0500

    [install.sh] init

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+ln -s ~/dotfiles/.config/nvim ~/.config/nvim
+ln -s ~/dotfiles/.config/zsh ~/.config/zsh
+ln -s ~/dotfiles/.config/alias ~/.config/alias
+ln -s ~/dotfiles/.config/profile ~/.config/profile
+
+ln -s ~/dotfiles/.zshrc ~/.zshrc
+ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
