0e3902a3db383bd289ada3221913b2780c256e57
jhkim19940830 <jhkim19940830@gmail.com>
2025-02-11 18:23:37 -0500

    [conform] disable format on save. keymap space + f to format

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -1,3 +1,5 @@
+local mapKey = require("utils.keyMapper").mapKey
+
 return {
 	"stevearc/conform.nvim",
 	event = { "BufReadPre", "BufNewFile" },
@@ -12,10 +14,14 @@ return {
 				cpp = { "clang-format" },
 				java = { "google-java-format" },
 			},
-			format_on_save = {
-				timeout_ms = 500,
-				lsp_fallback = true,
-			},
+			-- format_on_save = {
+			-- 	timeout_ms = 500,
+			-- 	lsp_fallback = true,
+			-- },
 		})
+
+		mapKey("<leader>f", function()
+			require("conform").format({ async = true, lsp_fallback = true })
+		end)
 	end,
 }
